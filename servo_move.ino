dac5958bd20f0322fce7902ebfbc930f12486db7
Faust-Lee <80118126+Faust-Lee@users.noreply.github.com>
2024-05-10 23:19:01 +0900

    Update servo_move.ino

--- servo_move.ino
+++ servo_move.ino
@@ -4,10 +4,7 @@ SMS_STS servo;  //635~3484
 #define l2 150 //unit: mm
 #define l3 100
 
-int x = 0, y = 1, z = 2;
-float pose[3] = { 60, 120, 90 };
-float deg[2][2];
-float d, D, theta1, theta2_1, theta2_2;
+float current_deg[2]={90,0};
 
 
 void setup() {
@@ -23,19 +20,8 @@ void setup() {
 
 void loop() {
 
-  d = sqrt(pow(pose[x], 2) + pow(pose[y], 2));
-  D = (pow(d, 2) - pow(l2, 2) - pow(l3, 2)) / (2.0 * l2 * l3);
-  deg[1][0] = atan2(sqrt(1 - pow(D, 2)) , D);                                                                                      // theta2_1
-  deg[1][1] = atan2(-sqrt(1 - pow(D, 2)) , D);                                                                                     // theta2_2
-  deg[0][0] = atan2(pose[y] , pose[x]) - atan2(l3 * sin(deg[1][0]) , l2 + l3 * cos(deg[1][0]));                    // theta1_1
-  deg[0][1] = atan2(pose[y] , pose[x]) - atan2(l3 * sin(deg[1][1]) , l2 + l3 * cos(deg[1][1]));                    // theta1_2
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
-      deg[i][j] *= (180 / PI);
-      deg[i][j] = (i == 0 ? map (deg[i][j], 0, 180, 3072, 1024) : map (deg[i][j], -90, 90, 3072, 1024));
-    }
-  }
-
+  
+  
 
   Serial.print("1번: ");
   Serial.print(deg[0][0]);
@@ -48,4 +34,47 @@ void loop() {
   Serial.println(deg[1][1]);
   Serial.println();
   delay(1000);
+}
+
+void servo_move2(int x, int y){
+  bool theta1=1,theta2=1;
+  float target_deg[2],deg[2][2];
+  float d = sqrt(pow(x, 2) + pow(y, 2));
+  float D = (pow(d, 2) - pow(l2, 2) - pow(l3, 2)) / (2.0 * l2 * l3);
+  
+  deg[1][0] = atan2(sqrt(1 - pow(D, 2)) , D);                                                          // theta2_1
+  deg[1][1] = atan2(-sqrt(1 - pow(D, 2)) , D);                                                         // theta2_2
+  deg[0][0] = atan2(y , x) - atan2(l3 * sin(deg[1][0]) , l2 + l3 * cos(deg[1][0]));                    // theta1_1
+  deg[0][1] = atan2(y , x) - atan2(l3 * sin(deg[1][1]) , l2 + l3 * cos(deg[1][1]));                    // theta1_2
+  for (int i = 0; i < 2; i++)
+    for (int j=0;j<2;j++)
+      deg[i][j] *= (180 / PI);
+  
+  if (deg[0][0]<0||180<deg[0][0]||-90>deg[1][0]||90<deg[1][0]) theta1 = 0;
+  if(deg[0][1]<0||180<deg[0][1]||-90>deg[1][1]||90<deg[1][1]) theta2 = 0;
+  
+  if(theta1&&theta2){
+    if( abs(current_deg[0]-deg[0][0])+abs(current_deg[1]-deg[1][0]) < abs(current_deg[0]-deg[0][1])+abs(current_deg[1]-deg[1][1])){
+      target_deg[0] = deg[0][0];
+      target_deg[1] = deg[1][0];
+    }
+    else{
+      target_deg[0] = deg[0][1];
+      target_deg[1] = deg[1][1];
+    }
+  }
+  else if(theta1){
+      target_deg[0] = deg[0][0];
+      target_deg[1] = deg[1][0];
+  }
+  else if(theta2){
+      target_deg[0] = deg[0][1];
+      target_deg[1] = deg[1][1];
+  }
+  delta_theta1=abs(map (current_deg[0], 0, 180, 3072, 1024)-map (target_deg[0], 0, 180, 3072, 1024));
+  delta_theta2=abs(map (current_deg[1], 0, 180, 3072, 1024)-map (target_deg[1], 0, 180, 3072, 1024));
+
+  servo.WritePosEx(9, map (target_deg[0], 0, 180, 3072, 1024), 2400, 50);
+  
+  servo.WritePosEx(11, map (target_deg[1], -90, 90, 3072, 1024), 2400, 50);
 }
